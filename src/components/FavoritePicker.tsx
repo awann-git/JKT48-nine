7b5d1f37426f85048e6337ab32d54a2844f04fb6
Awan <novaawan66@gmail.com>
2026-03-26 15:47:02 +0700

    Simplify gradient colors in FavoritePicker
    Removed various gradient color options and replaced them with repeated 'from-rose-500 to-pink-600'.

--- src/components/FavoritePicker.tsx
+++ src/components/FavoritePicker.tsx
@@ -15,14 +15,14 @@ const GRID_SIZE = 9;
 
 const GRADIENTS = [
   "from-rose-500 to-pink-600",
-  "from-amber-500 to-orange-600",
-  "from-emerald-500 to-teal-600",
-  "from-sky-500 to-blue-600",
-  "from-violet-500 to-purple-600",
-  "from-fuchsia-500 to-pink-600",
-  "from-lime-500 to-green-600",
-  "from-cyan-500 to-teal-600",
-  "from-red-500 to-rose-600",
+  "from-rose-500 to-pink-600",
+  "from-rose-500 to-pink-600",
+  "from-rose-500 to-pink-600",
+  "from-rose-500 to-pink-600",
+  "from-rose-500 to-pink-600",
+  "from-rose-500 to-pink-600",
+  "from-rose-500 to-pink-600",
+  "from-rose-500 to-pink-600",
 ];
 
 const FavoritePicker = () => {
